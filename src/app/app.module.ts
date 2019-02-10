b5b133f7163cd3287663da76611e8e4bae705185
mpisters <pistersmichelle@gmail.com>
2019-02-10 21:15:17 +0100

    start of the vacancyapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ApplicationFormComponent } from './application-form/application-form.component';
+import { ApplicationPageComponent } from './application-page/application-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ApplicationFormComponent,
+    ApplicationPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  openMenu(): void {
+    const navs = document.querySelectorAll('.navbar__items');
+    navs.forEach(nav => nav.classList.toggle('navbar__toggleShow'));
+  }
+
+}
